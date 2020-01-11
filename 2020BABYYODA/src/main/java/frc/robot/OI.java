ae8e6ac5228551182103f878ae7a473f0680da64
Zaderman0411 <zaderman0411@gmail.com>
2020-01-11 16:01:51 -0500

    Initial Code
    Great Stuff ~ Iskandar Nazhar

---
+++ 2020BABYYODA/src/main/java/frc/robot/OI.java
@@ -0,0 +1,78 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018-2019 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+import frc.robot.Constants;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.XboxController.Button;
+/** import edu.wpi.first.hal.sim.mockdata.DriverStationDataJNI; import edu.wpi.first.wpilibj.Joystick;*/
+
+
+public class OI {
+
+    public Button[] xboxButton_array = new Button[11];
+    public XboxController xbox = new XboxController(Constants.XBOX_PORT);
+
+    /** Contructer */
+
+    public OI() {
+
+    
+        /**	Fill in the Xbox Array
+         * 
+         *  for(int i = 1; i < xboxButton_array.length; i++) {
+			    xboxButton_array[i] = new JoystickButton(xbox, i);	
+		    }
+        */
+	
+
+
+    }
+    /**   Xbox Joysticks */    
+
+
+    public double getXboxLeftX() {
+		return xbox.getX(GenericHID.Hand.kLeft);
+	}
+
+    public double getXboxLeftY() {
+		return xbox.getY(GenericHID.Hand.kLeft);
+	}
+
+    public double getXboxRightX() {
+		return xbox.getX(GenericHID.Hand.kRight);
+	}
+
+    public double getXboxRightY() {
+		return xbox.getY(GenericHID.Hand.kRight);
+
+    }
+
+    /** Xbox Triggers */
+
+    public double getXboxRightTrigger() {
+        return xbox.getTriggerAxis(GenericHID.Hand.kRight);
+        }
+
+    public double getXboxLeftTrigger() {
+        return xbox.getTriggerAxis(GenericHID.Hand.kLeft);
+        }
+
+    /** RUMBLE CODE
+    public void setXboxRumble(double power) {
+        xbox.setRumble(GenericHID.RumbleType.kLeftRumble, power);
+        xbox.setRumble(GenericHID.RumbleType.kRightRumble, power);
+        }
+    */
+
+
+
+
+
+
+}
